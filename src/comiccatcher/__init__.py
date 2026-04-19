fdaaa27c2d6b75b887e86f237cf6ee754afa8f9d
Tony Beville <beville@alum.wpi.edu>
2026-04-19 12:16:28 -0700

    Bump version again to test github auto workflow

--- src/comiccatcher/__init__.py
+++ src/comiccatcher/__init__.py
@@ -4,7 +4,7 @@
 import subprocess
 from pathlib import Path
 
-__version__ = "0.1.0a6"
+__version__ = "0.1.0a7"
 
 def get_version_string() -> str:
     """Returns the base version, optionally appended with git build info."""
